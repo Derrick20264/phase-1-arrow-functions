a0d030625017d9d75ed8a87e8d4314a127469ea6
derrick <derrick.mwangi2@student.moringaschool.com>
2025-06-06 23:22:15 +0300

    feat: complete function expression and arrow function definitions

---
+++ index.js
@@ -0,0 +1,10 @@
+// Function expression called divide
+const divide = function() {
+  return 2000 / 100;
+};
+
+// Arrow function called square (takes one parameter)
+const square = x => x * x;
+
+// Arrow function called add (takes two parameters)
+const add = (a, b) => a + b;
